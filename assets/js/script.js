fc61cf368f3e768ec511e325c8b31afa8f80c919
alondra <alondra9020@gmail.com>
2022-02-19 12:33:13 -0500

    add handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to pass once the Add Task button is clicked
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefaul();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 // Event listener for the Ad Task button
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("click", createTaskHandler);
